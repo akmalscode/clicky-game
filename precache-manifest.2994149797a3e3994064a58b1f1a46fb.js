2c9619cc37a191f62fdf103d56eb3b1b817f0c0b
Md Akmal Hossain <akmal2080@gmail.com>
2019-07-25 22:57:58 -0400

    Updates

---
+++ precache-manifest.2994149797a3e3994064a58b1f1a46fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd178a20d68b6065ee8ef8a946f5add",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "664c1ba427c4755e1bea",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/clicky-game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "664c1ba427c4755e1bea",
+    "url": "/clicky-game/static/js/main.d1a70cfd.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
